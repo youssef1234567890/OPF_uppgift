434d04ac2e19dc8bd7c64b8037fe55af55a80d23
youssef1234567890 <kgayoussefbs@gmail.com>
2025-03-04 19:13:32 +0100

    html css changes

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8507d76e5793161ff834c1bd91f5c03ce7f5ccec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc8e9e81ddda922aaac6ab777aacceafd7515a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
