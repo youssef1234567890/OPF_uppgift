062dbd23c113277647d0683df656f2b30dfae048
youssef1234567890 <kgayoussefbs@gmail.com>
2025-03-05 11:45:48 +0100

    messige system 90% done

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d603088f8f231e08849de4c47bc0746d6ad48fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ecf543703246f8ed0138f34ca155b7ea33dd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
