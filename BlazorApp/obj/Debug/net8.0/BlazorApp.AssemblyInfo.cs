fa26a152e0998c05d9c9614205b351febfa639bd
youssef1234567890 <kgayoussefbs@gmail.com>
2025-03-07 12:16:13 +0100

    fixed log with dilin

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afb4bfc62fd93366f9555ab95f9a29044eff6e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f584f1ff9b6739f50cc5d79f9f785be818230e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
