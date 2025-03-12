e4cb1d324d2fd242327789821e2cd26450dbd990
Qwanie <143743954+Qwanie@users.noreply.github.com>
2025-03-12 23:49:44 +0100

    Updated Home page, updated Log page and removed files that are no longer needed.

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b67638776c493b221800222c9e1f9f496c89895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed645dd994842b461be8d1a1ae64d824721505db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
