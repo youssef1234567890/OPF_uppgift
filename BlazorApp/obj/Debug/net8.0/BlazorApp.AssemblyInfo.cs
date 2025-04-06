1070dc9aa868b5e63616577cfe5611b46e3e0b66
youssef1234567890 <kgayoussefbs@gmail.com>
2025-04-06 11:28:18 +0200

    SOC catogire

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e933b204073af93204f57caafd62ecb709dbd0d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2da3c5db7d146e0e695dae7678e65ebb68cdd74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
