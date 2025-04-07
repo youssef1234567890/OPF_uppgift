ccb360529087581d76846d1b5361fbbbac947cef
Qwanie <143743954+Qwanie@users.noreply.github.com>
2025-04-07 15:51:08 +0200

    Seperated css design to wwwroot/css and worked on the design of the pages.

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2da3c5db7d146e0e695dae7678e65ebb68cdd74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1070dc9aa868b5e63616577cfe5611b46e3e0b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
